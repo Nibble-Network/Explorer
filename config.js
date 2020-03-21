fc81423ae40e0894859c126ca3e79463c5a39d1c
Sups <36714847+Sudosups@users.noreply.github.com>
2020-03-21 16:02:33 +0000

    https

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-var api = 'http://explorecrumbs.pw:16001'; //to be updated for crumbs
-var donationAddress = "Nib1XAX8nHgAZzSDoCjd3mAa2di7dGaXh9u9QFVz9tWjCF7iJqTsU23A4r5EcxYw6Z9jZL93Nfob6C57LyiW26VXC57U3tPLbPphpyoUftnh8xVqcVbZYcXY3ahDX7qPo63HKkEfE6gPk3jBDX1vZW5JwxgixXTwz9NGqBP23SeFoypw9iB13Xjn2g";
+var api = 'https://explorecrumbs.pw:16001'; //to be updated for crumbs
+var donationAddress = "Nib1ZHnXbBfUYU5F6VFpVyZEHAQwYW7q5AMEqHhotUY8MgjfpJjshas8PjMCfNpqKs18xxqzjVsbYPr1G2F8NXDL5ff9LUQTRf";
 var blockTargetInterval = 120; 		// enter the block interval in seconds
 var coinUnits = 1000000;  			// enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
 var totalSupply =  21000000000000; 	// enter the total supply in atomic units
