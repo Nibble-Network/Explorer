cbab0dbdb155b3fe33d8349098a375e9c232dbe3
Sups <36714847+Sudosups@users.noreply.github.com>
2020-03-21 07:10:09 -0700

    port

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://explorecrumbs.pw/api'; //to be updated for crumbs
+var api = 'http://explorecrumbs.pw:16001'; //to be updated for crumbs
 var donationAddress = "Nib1XAX8nHgAZzSDoCjd3mAa2di7dGaXh9u9QFVz9tWjCF7iJqTsU23A4r5EcxYw6Z9jZL93Nfob6C57LyiW26VXC57U3tPLbPphpyoUftnh8xVqcVbZYcXY3ahDX7qPo63HKkEfE6gPk3jBDX1vZW5JwxgixXTwz9NGqBP23SeFoypw9iB13Xjn2g";
 var blockTargetInterval = 120; 		// enter the block interval in seconds
 var coinUnits = 1000000;  			// enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
