5128cc4738fa6742b570748205689f171d357c10
Sups <36714847+Sudosups@users.noreply.github.com>
2020-05-17 08:40:10 -0700

    should be corrected for reward to display

--- q/reward/index.php
+++ q/reward/index.php
@@ -5,6 +5,6 @@ $config = (require '../../config.php');
 $blockHeader = fetch_rpc($config['api'], 'getlastblockheader', '""');
 
 $rewardRaw = $blockHeader['result']['block_header']['reward'];
-$reward = number_format($rewardRaw / 100, 2, ".", "");
+$reward = number_format($rewardRaw / 1000000, 6, ".", "");
 
 print_r($reward);
