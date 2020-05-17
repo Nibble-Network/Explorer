a33903fd4c5f849e0445fc8bc6f92228292bab06
Sups <36714847+Sudosups@users.noreply.github.com>
2020-05-17 08:21:30 -0700

    Preperation for changes
    Updated donation address to Mad's address
    Updated to NBXC

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
-var api = 'https://explorecrumbs.pw:16001'; //to be updated for crumbs
-var donationAddress = "Nib1ZHnXbBfUYU5F6VFpVyZEHAQwYW7q5AMEqHhotUY8MgjfpJjshas8PjMCfNpqKs18xxqzjVsbYPr1G2F8NXDL5ff9LUQTRf";
+var api = 'https://crumbs.hopto.org:16001'; //to be updated for Nibble
+var donationAddress = "XCRCuXroBTLFqF4UXuSRdrTNjwFpBU47V2NY1xPDZyEXY1czfGMrKwSVBoMgCd7jVBR9eQnrov2VBNFgQ5rbZyiu731u4hcRpY"; //Mad's donation address
 var blockTargetInterval = 120; 		// enter the block interval in seconds
 var coinUnits = 1000000;  			// enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
 var totalSupply =  21000000000000; 	// enter the total supply in atomic units
-var symbol = 'XCR'; 			// enter the coin's ticker
+var symbol = 'NBXC'; 			// enter the coin's ticker
 var refreshDelay = 90000;
 
 // pools stats by MainCoins
 var networkStat = {
-"XCR": [
+"NBXC": [
 //	["cnpool.cc", "https://cnpool.cc/api/nbx/"],
 // 	["nibbleclassic.my-mining-pool.de", "https://apinibbleclassic.my-mining-pool.de/"]
  ]
 };
 
 var networkStat2 = {
- "XCR": [
+ "NBXC": [
 //	["webnbx.semipool.com", "https://webnbx.semipool.com/api"]
  ]
 };
