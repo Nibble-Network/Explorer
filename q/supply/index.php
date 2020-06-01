784091d924a45b0fe13baffd11327c1a3341bd7f
MaddestHatter <44808557+MaddestHatter@users.noreply.github.com>
2020-06-01 08:45:40 +0200

    supply = total odd invested
    didn´t i do this ?

--- q/supply/index.php
+++ q/supply/index.php
@@ -3,12 +3,16 @@ require '../util.php';
 $config = (require '../../config.php');
 
 $info = fetch_rpc($config['api'], 'getlastblockheader', '""');
-$hash = $info['result']['block_header']['hash'];
 
+$hash = $info['result']['block_header']['hash'];
 $blockData = fetch_rpc($config['api'], 'f_block_json', '{"hash":"'.$hash.'"}');
 $supplyRaw = $blockData['result']['block']['alreadyGeneratedCoins'];
 
-$supply = number_format($supplyRaw / $config['coinUnits'], 6, ".", "");
+$info = fetch_getinfo($config['api']);
+$depositsRaw = $info['full_deposit_amount'];
+
+$supply = number_format(($supplyRaw - $depositsRaw) / $config['coinUnits'], 6, ".", "");
 
 print_r($supply);
 ?>
+
