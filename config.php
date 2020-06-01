a4ba6170cfb1716538dd19a1da94e61cf6af3326
MaddestHatter <44808557+MaddestHatter@users.noreply.github.com>
2020-06-01 22:52:15 +0200

    add maxSupply atomic--> aka total ever be there

--- config.php
+++ config.php
@@ -2,5 +2,6 @@
 return array(
         'api' => 'http://127.0.0.1:16001',
         'blockTargetInterval' => 120,
-        'coinUnits' => 1000000
+        'coinUnits' => 1000000,
+        'totalSupply' => 21000000000000
 );
