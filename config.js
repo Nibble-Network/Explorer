ff8eba3552b45da45d3fd3f9592c0e38a7649f1e
Sups <36714847+Sudosups@users.noreply.github.com>
2020-12-07 18:38:42 +0000

    nibbling.org

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-var api = 'https://crumbs.hopto.org:16001'; //to be updated for Nibble
-var donationAddress = "XCRCuXroBTLFqF4UXuSRdrTNjwFpBU47V2NY1xPDZyEXY1czfGMrKwSVBoMgCd7jVBR9eQnrov2VBNFgQ5rbZyiu731u4hcRpY"; //Mad's donation address
+var api = 'https://nibbling.org:16001'; //to be updated for Nibble
+var donationAddress = "XCRD9dvfLD6UBWFFpS141vHRgSEt8hk77YLr5a91QaK8KqRHMTUQzPs3wiZ3wNUkZYb5eZxbkaS7WH1YuUVXU9qh96Vxs8muL8"; //Sups donation address
 var blockTargetInterval = 120; 		// enter the block interval in seconds
 var coinUnits = 1000000;  			// enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
 var totalSupply =  21000000000000; 	// enter the total supply in atomic units
